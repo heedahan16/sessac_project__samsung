f68e3ddb04f7c272f2b88b631683d5d10930a839
heedahan16 <devhee.handa@gmail.com>
2023-09-17 06:00:35 +0900

    삼성전자 네이버뉴스 데이터 크롤링 후 연합뉴스 데이터 출력 실패

--- news/news.py
+++ news/news.py
@@ -4,15 +4,26 @@ from bs4 import BeautifulSoup
 headers = {
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
 }
-response = requests.get("https://news.naver.com/main/list.nhn?mode=LPOD&mid=sec&oid=001", headers=headers)
-beautiful = BeautifulSoup(response.text, "html.parser")
+
+response = requests.get("https://news.naver.com/main/officeList.naver", headers=headers)
+beautiful = (response.text, "html.parser")
+
+print(beautiful)
+
+oid = "001"
+
+respon = requests.get("https://news.naver.com/main/list.nhn?mode=LPOD&mid=sec&oid=" + oid, headers=headers)
+soup = BeautifulSoup(respon.text, "html.parser")
+
+
+print(soup.select("div#groupOfficeList table.group_table"))
 
 for day in range(1, 32):
     if day < 10:
         day = "0" + str(day)
     date = "202308" + str(day)
-    print(date)
-    respon = requests.get("https://news.naver.com/main/list.naver?mode=LPOD&mid=sec&oid=001&date=" + str(date), headers=headers)
-    soup = BeautifulSoup(respon.text, "html.parser")
     
-    print(soup)
+    res = requests.get("https://news.naver.com/main/list.naver?mode=LPOD&mid=sec&oid=001&date=" + str(date), headers=headers)
+    bs = BeautifulSoup(res.text, "html.parser")
+    
+    # print(bs.select("ul.type06_headline li dl"))
