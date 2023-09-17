639d47e9797972055de3d5fc3a0ba5673397f5ba
heedahan16 <devhee.handa@gmail.com>
2023-09-18 08:57:48 +0900

    네이버뉴스 연합뉴스 데이터 크롤링 후 2023년 8월 전체 기사 전체 페이지 불러오기 성공

--- news/news.py
+++ news/news.py
@@ -48,9 +48,10 @@ for date in range(1, 32):
     print("date: ", date)
 
     page = 1
-    print("page: ", page)
 
     while True:
+
+        print("page: ", page)
 
         params = {
             "mode":"LPOD"
@@ -63,36 +64,36 @@ for date in range(1, 32):
         respon = requests.get("https://news.naver.com/main/list.naver", params=params, headers=headers)
         soup = BeautifulSoup(respon.text, "html.parser")
 
-        now_page = soup.select_one("div.paging strong").text
+        now_page = int(soup.select_one("div.paging strong").text)
 
-        if now_page != page:
+        if page != now_page:
             break
 
-    for dl in soup.select("div.list_body ul li dl"):
-        if dl.select_one("dt.photo a") != None:
-            article = dl.select_one("dt.photo a").attrs["href"]
-        else:
-            article = dl.select_one("dt a").attrs["href"]
+        for dl in soup.select("div.list_body ul li dl"):
+            if dl.select_one("dt.photo a") != None:
+                article = dl.select_one("dt.photo a").attrs["href"]
+            else:
+                article = dl.select_one("dt a").attrs["href"]
 
-        res = requests.get(article, headers=headers)
-        bs = BeautifulSoup(res.text, "html.parser")
+            res = requests.get(article, headers=headers)
+            bs = BeautifulSoup(res.text, "html.parser")
 
-        try:
-            title = bs.select_one("div#ct h2").text
-            content = bs.select_one("div#contents article#dic_area")
-        except:
             try:
-                title = bs.select_one("div#content h4").text
-                content = bs.select_one("div#content div#newsEndContents")
+                title = bs.select_one("div#ct h2").text
+                content = bs.select_one("div#contents article#dic_area")
+                
             except:
-                title = bs.select_one("div#content h2").text.replace("\n", "").strip()
                 try:
-                    content = bs.select_one("div.end_body_wrp div#articBody")
+                    title = bs.select_one("div#content h4").text
+                    content = bs.select_one("div#content div#newsEndContents")
                 except:
-                    content = bs.select_one("div.end_body_wrp")
+                    title = bs.select_one("div#content h2").text.replace("\n", "").strip()
+                    try:
+                        content = bs.select_one("div.end_body_wrp div#articBody")
+                    except:
+                        content = bs.select_one("div.end_body_wrp")
 
-        reg = "[a-zA-Z]* *ⓒ.*|<br\/><br\/> * |[a-zA-Z0-9]*@.*|<[^>]*>"
-        content_reg = re.sub(reg, "", str(content)).replace("\n", "").strip()
-        print(content_reg)
+            reg = "[a-zA-Z]* *ⓒ.*|<br\/><br\/> * |[a-zA-Z0-9]*@.*|<[^>]*>"
+            content_reg = re.sub(reg, "", str(content)).replace("\n", "").strip()
 
-    page += 1
+        page += 1
